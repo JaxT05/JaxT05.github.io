eb64c37d6c831ee9f766c0d756e6f928bcde18f3
Jax Tobyas <153058960+JaxT05@users.noreply.github.com>
2026-03-19 21:02:51 -0400

    please

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/JaxT05.github.io/",
+  base: "https://JaxT05.github.io/",
   plugins: [vue()],
 })
